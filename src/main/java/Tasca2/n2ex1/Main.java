d67bb7e6a15fbe15e02b3688feaa4f6d3ef9dabd
David Carcelen <davidcarcelen@MacBook-Pro-de-David-2.local>
2024-06-04 13:20:42 +0200

    mejoras Main

--- src/main/java/Tasca2/n2ex1/Main.java
+++ src/main/java/Tasca2/n2ex1/Main.java
@@ -1,16 +1,13 @@
 package Tasca2.n2ex1;
 
 public class Main {
+    static Ticket pago1 = new Ticket("Unos zapatos", 23.50, PaymentMethod.PAYPAL);
+    static Ticket pago2 = new Ticket("Unas bambas", 48.50, PaymentMethod.BANK);
+    static Ticket pago3 = new Ticket("Unos calcetines", 7.00, PaymentMethod.CREDITCARD);
+    static String result;
+    static EventListener listener = new PasarelaDePago();
+    static Zapateria zapateria = new Zapateria(listener);
     public static void main(String[] args) {
-
-        Ticket pago1 = new Ticket("Unos zapatos", 23.50, PaymentMethod.PAYPAL);
-        Ticket pago2 = new Ticket("Unas bambas", 23.50, PaymentMethod.BANK);
-        Ticket pago3 = new Ticket("Unos calcetines", 23.50, PaymentMethod.CREDITCARD);
-
-        EventListener listener = new PasarelaDePago();
-        Zapateria zapateria = new Zapateria(listener);
-
-        String result;
 
         result = zapateria.operation(pago1);
         System.out.println("Callback realizado, " + result + "\n");
